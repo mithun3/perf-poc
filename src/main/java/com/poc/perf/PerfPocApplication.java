95c5f9add93259a08390baca2791e168d085d9a1
P S <617150@cognizant.com>
2018-06-22 11:16:09 +1000

    initial commit - spring boot with certain dependencies

---
+++ src/main/java/com/poc/perf/PerfPocApplication.java
@@ -0,0 +1,12 @@
+package com.poc.perf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfPocApplication.class, args);
+	}
+}
